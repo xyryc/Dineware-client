4388471a3fa52130023a6cd0eb7aaa9d24f581d3
xyryc <amdanikzz@gmail.com>
2024-12-21 23:56:26 +0600

    project setup and added routes+navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,86 @@
+const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+
+      <li>
+        <a>All Foods</a>
+      </li>
+
+      <li>
+        <a>Gallery</a>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">Dineware</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
+
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a>My Foods</a>
+            </li>
+            <li>
+              <a>Add food</a>
+            </li>
+            <li>
+              <a>My Orders</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar";
+
+const Main = () => {
+  return <div>
+    <Navbar/>
+  </div>;
+};
+
+export default Main;
