1836bc2626d152b7bdd96b2f6475d6f14fead6c5
xyryc <amdanikzz@gmail.com>
2024-12-24 10:42:36 +0600

    added interceptor for axios requests

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}`,
@@ -6,6 +9,35 @@ const axiosInstance = axios.create({
 });
 
 const useAxiosSecure = () => {
+  const { logOut } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axiosInstance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log("error caught in interceptor", error);
+
+        if (error.status === 401 || error.status === 403) {
+          console.log("need to logout  user");
+
+          logOut()
+            .then(() => {
+              console.log("logout user");
+              navigate("/login");
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+
+        return Promise.reject(error);
+      }
+    );
+  }, []);
+
   return axiosInstance;
 };
 
